74ec6ca7f52c193e851a86df3fc1cb00a21e9174
HeirToTheShadows <smdh88@gmail.com>
2025-05-29 11:21:56 -0600

    Ch2 L5: Refactor

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ def main():
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+from stats import get_num_words
 
 
 main()
