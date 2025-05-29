2dadeaba2649818a0649ae8f6fb47aece78a0391
HeirToTheShadows <smdh88@gmail.com>
2025-05-29 16:38:09 -0600

    Ch 2 L6 Remove Print Brackets

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     num_words = get_num_words(text)
     char_count = get_char_dict(text)
     print(f"{num_words} words found in the document.")
-    print ({char_count})
+    print (char_count)
 
 
 def get_book_text(path):
