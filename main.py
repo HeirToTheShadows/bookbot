adcaffe6e6109c9ca374931e9ac62c6bf7b8dde0
HeirToTheShadows <smdh88@gmail.com>
2025-05-29 14:27:47 -0600

    Ch 2 L 6 3rd atmpt

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document.")
-    print (char_list)
+    print (char_count)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 from stats import get_num_words
-from stats import char_list
+from stats import char_count
 
 
 main()
