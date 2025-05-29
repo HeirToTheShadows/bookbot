a82df834946b0ec434968039b33129c61356c806
HeirToTheShadows <smdh88@gmail.com>
2025-05-29 14:24:54 -0600

    C2 L 6 Char Count 2nd atmpt

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document. {char_list}")
+    print(f"{num_words} words found in the document.")
+    print (char_list)
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -1,12 +1,16 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
-def char_list(text):
-    char_list = text.split()
+
+def char_list(words):
+    char_list = words.split()
     return char_list
+
 def char_count(char_list):
     char_dict = {}
     for character in char_list:
-        if character in char_dict:
-            char_dict[character] =+ 1
+        c = character.lower()
+        if c in char_dict:
+            char_dict[c] += 1
+        else: char_dict[c] = 1
     return char_dict
