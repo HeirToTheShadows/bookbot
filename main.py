4cc0f0de26a4bc164aedd20bf3fc609b71d7c68e
HeirToTheShadows <smdh88@gmail.com>
2025-05-29 16:29:53 -0600

    Ch2 L6 Counting Char Fix Output

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 def main():
     from stats import get_num_words
+    
     from stats import get_char_dict
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count = get_char_dict(text)
     print(f"{num_words} words found in the document.")
-    print (f"Character count: {char_count}")
+    print ({char_count})
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+if __name__ == "__main__":
 
 
 
-main()
+    main()
