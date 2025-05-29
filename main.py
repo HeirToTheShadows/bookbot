e617a79a336e7d0ab520662ce7be84d4840a7074
HeirToTheShadows <smdh88@gmail.com>
2025-05-29 11:16:49 -0600

    Updating to match Bootdev

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+
 def get_book_text(path):
     with open(path) as f:
-        file_contents = f.read()
-        return file_contents
-def count_text(file_contents):
-    total_words = 0
-    word_list = file_contents.split()
-    total_words = len(word_list)
-    return total_words
-def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    total_words = count_text(book_text)
-    print (f"{total_words} words found in the document.")
+        return f.read()
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
 
 main()
