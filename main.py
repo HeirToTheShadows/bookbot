e411d2e9cb7886336a60fc1e07a3bcbf9a1206ef
HeirToTheShadows <smdh88@gmail.com>
2025-05-29 14:43:18 -0600

    Ch 2 L6: Count Char Pass?

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    char_count = get_char_dict(text)
     print(f"{num_words} words found in the document.")
-    print (char_count)
+    print (f"Character count: {char_count}")
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 from stats import get_num_words
-from stats import char_count
+from stats import get_char_dict
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,15 +2,13 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def char_list(words):
-    char_list = words.split()
-    return char_list
+def get_char_dict(text):
+    char = {}
+    for c in text.lower():
+        if c in char:
+            char[c] += 1
+        else: char[c] = 1
+    return char
 
-def char_count(char_list):
-    char_dict = {}
-    for character in char_list:
-        c = character.lower()
-        if c in char_dict:
-            char_dict[c] += 1
-        else: char_dict[c] = 1
-    return char_dict
+
+   
