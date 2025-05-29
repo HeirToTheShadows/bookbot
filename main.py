ded1935b1fba6f45a24dc9d80f31f7f28625af81
HeirToTheShadows <smdh88@gmail.com>
2025-05-29 10:43:09 -0600

    Ch 2 L4 Word Count

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    total_words = count_text(book_text)
-    return book_text, total_words
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
@@ -10,5 +6,10 @@ def count_text(file_contents):
     total_words = 0
     word_list = file_contents.split()
     total_words = len(word_list)
-    print(f"{total_words} words found in the document.")
+    return total_words
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    total_words = count_text(book_text)
+    print (f"{total_words} words found in the document.")
+
 main()
