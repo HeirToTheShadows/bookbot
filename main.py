daf9cc0d309793271eb49ebf3971c51f867bf90c
HeirToTheShadows <smdh88@gmail.com>
2025-05-31 13:40:11 -0600

    CH3 L1: Report

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count = get_char_dict(text)
-    dict = sort_ditct(char_count)
-    print(f"============ BOOKBOT ============'\n'Analyzing book found at books/frankenstein.txt...'\n'{dict}'\n' Found {num_words} total words '\n'{dict}")
+    char_list= sort_ditct(char_count)
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print(f"--------- Character Count -------")
+    for c in char_list:
+        print(f"{c['name']}: {c['num']}") 
+    print(f"============= END ===============")
+
+   
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -9,12 +9,14 @@ def get_char_dict(text):
             char[c] += 1
         else: char[c] = 1
     return char
+
 def sort_ditct(char):
-    #sort by value
-    import operator
-    sorted = dict(sorted(char.items(), key=lambda item: item [1], reverse=True))
-    print(char)
-
-
-
-   
+    sorted_char = []
+    for character, count in char.items():
+            if character.isalpha():
+             alpha_dict = {"name":character,"num":count}
+             sorted_char.append(alpha_dict)
+    def sort_on(sorted_char):
+        return sorted_char["num"]
+    sorted_char.sort(reverse=True, key=sort_on) 
+    return sorted_char 
