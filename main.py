6fb34fce9897cefa2b891ae6903514fff95a26fc
HeirToTheShadows <smdh88@gmail.com>
2025-06-01 10:56:15 -0600

    Ch3 L2: Arguments

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from stats import get_num_words, get_char_dict, sort_ditct
 import sys
 
 def main():
+    if len(sys.argv)<2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count = get_char_dict(text)
     char_list= sort_ditct(char_count)
-    print(f"Usage: python3 main.py <path_to_book>")
     print(f"============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}")
     print(f"----------- Word Count ----------")
